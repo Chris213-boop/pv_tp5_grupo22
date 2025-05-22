912c4545a016bcdae69a1079c8b7e77d0bddf996
Chris213-boop <46472499@fi.unju.edu.ar>
2025-05-22 17:03:31 -0300

    Primer commit inicialización TP05 Torres Cintia Cristina

---
+++ src/assets/components/App.jsx
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <>
+    <h1>PROGRAMACIÓN VISUAL</h1>
+    <h2>Asistencia 22/05/2025</h2>
+    <ul>
+      <li> </li>
+      <li> </li>
+      <li> </li>
+      <li> </li>
+      <li> </li>
+    </ul>
+    </>
+  )
+}
+
+export default App
